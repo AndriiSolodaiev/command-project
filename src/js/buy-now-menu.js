50155884f0db4d72c61d125898abe0c5d10c126f
kostya3004 <110536627+kostya3004@users.noreply.github.com>
2022-10-08 21:22:16 +0300

    Almost over
    Додав і підключив visually-hidden.scss в utils. Додав 2 скріпти в папку js. Ще кілька нюансів доробити і фініш.

--- src/js/buy-now-menu.js
+++ src/js/buy-now-menu.js
@@ -9,6 +9,6 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("buy-now-is-hidden");
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
