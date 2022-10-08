6157e03a0b8550bf8ce2e993c56f9f23b027b7d2
AndriiSolodaiev <109220078+AndriiSolodaiev@users.noreply.github.com>
2022-10-08 22:30:19 +0000

    Deploying to gh-pages from @ AndriiSolodaiev/command-project@fae3c035d633205e37ad0040f217ed894786a5cd 🚀

---
+++ index.6ede0cd7.js
@@ -0,0 +1,2 @@
+(()=>{const e={openMenuBtn:document.querySelector("[mobile-menu-open]"),closeMenuBtn:document.querySelector("[mobile-menu-close]"),menu:document.querySelector("[mobile-menu]")};function n(){e.menu.classList.toggle("mobile-menu-is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
+//# sourceMappingURL=index.6ede0cd7.js.map
---
+++ index.effd973f.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector("[mobile-menu-open]"),closeMenuBtn:document.querySelector("[mobile-menu-close]"),menu:document.querySelector("[mobile-menu]")};function n(){e.menu.classList.toggle("mobile-menu-is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.effd973f.js.map
