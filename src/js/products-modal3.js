42056cd9a99d3e87cccfb74d7419ec3b96f41cf0
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2022-10-09 17:45:50 +0300

    products modal

--- src/js/products-modal3.js
+++ src/js/products-modal3.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[products-modal-open]"),
-    closeModalBtn: document.querySelector("[products-modal-close]"),
-    modal: document.querySelector("[products-modal]"),
+    openModalBtn: document.querySelector("[products-modal3-open]"),
+    closeModalBtn: document.querySelector("[products-modal3-close]"),
+    modal: document.querySelector("[products-modal3]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -12,7 +12,3 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
-
-
-
-
