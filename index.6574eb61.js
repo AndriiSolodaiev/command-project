889feb58388e3fd5853b08ba9c1bbd1e64c16db3
AndriiSolodaiev <109220078+AndriiSolodaiev@users.noreply.github.com>
2022-10-10 16:43:45 +0000

    Deploying to gh-pages from @ AndriiSolodaiev/command-project@d1415996de32e3e31a8c07b166ddf0c20431afb9 🚀

---
+++ index.6574eb61.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[modal-franchise-open]"),closeModalBtn:document.querySelector("[modal-franchise-close]"),modal:document.querySelector("[franchise-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.6574eb61.js.map
