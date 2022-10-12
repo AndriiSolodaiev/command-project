f209e19900a44b2e9f6ed1494f26fb644de61271
AndriiSolodaiev <109220078+AndriiSolodaiev@users.noreply.github.com>
2022-10-12 16:59:01 +0000

    Deploying to gh-pages from @ AndriiSolodaiev/command-project@a88885814c42d529fe5905744c7a8d694ace728a 🚀

---
+++ index.7c08988a.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0}),$(".autoplay").slick({slidesToShow:1,autoplay:!0,autoplaySpeed:4e3});
+//# sourceMappingURL=index.7c08988a.js.map
--- index.c96ad3b5.js
+++ index.c96ad3b5.js
@@ -1,2 +1,2 @@
 $(".one-time").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0}),$(".autoplay").slick({slidesToShow:1,autoplay:!0,autoplaySpeed:4e3});
-//# sourceMappingURL=index.fae2063a.js.map
+//# sourceMappingURL=index.c96ad3b5.js.map
